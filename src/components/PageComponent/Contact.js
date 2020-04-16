219634d11b3745c30ab473d00d461c02cbe719cd
cymaniatico <ingluisgarciac@gmail.com>
2020-04-16 18:08:11 -0500

    Add form in contact component

--- src/components/PageComponent/Contact.js
+++ src/components/PageComponent/Contact.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    phone: '',
+                    email: '',
+                    comment: '',
+                }}
+                validationSchema={Yup.object().shape({
+                    firstName: Yup.string()
+                        .required('First Name is required'),
+                    lastName: Yup.string()
+                        .required('Last Name is required'),
+                    phone: Yup.string()
+                        .min(7, 'Number invalid')
+                        .required('Phone number is required'),
+                    email: Yup.string()
+                        .email('Email is invalid')
+                        .required('Email is required'),
+                    comment: Yup.string()
+                        .min(6, 'Comment must be at least 6 characters')
+                        .required('Comment is required'),
+                })}
+                onSubmit={fields => {
+                    alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
+                }}
+                render={({ errors, status, touched }) => (
+                    <Form>
+                        <div className="form-group">
+                            <label htmlFor="firstName">Nombres</label>
+                            <Field name="firstName" type="text" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '')} />
+                            <ErrorMessage name="firstName" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="lastName">Apellidos</label>
+                            <Field name="lastName" type="text" className={'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '')} />
+                            <ErrorMessage name="lastName" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="phone">Teléfono</label>
+                            <Field name="phone" type="number" className={'form-control' + (errors.phone && touched.phone ? ' is-invalid' : '')} />
+                            <ErrorMessage name="email" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Correo</label>
+                            <Field name="email" type="text" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
+                            <ErrorMessage name="email" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="comment">Mensaje</label>
+                            <Field name="comment" type="password" className={'form-control' + (errors.comment && touched.comment ? ' is-invalid' : '')} />
+                            <ErrorMessage name="comment" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary mr-2">Enviar</button>
+                            <button type="reset" className="btn btn-secondary">Borrar</button>
+                        </div>
+                    </Form>
+                )}
+            />
         );
     }
 }
