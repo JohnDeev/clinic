edbe85971e364346a5e8b194a37d0267b23bb120
cymaniatico <ingluisgarciac@gmail.com>
2020-05-29 17:08:22 -0500

    Install Redux. Remove Animated Transition.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-
+//import {createStore} from 'redux';
 import 'react-chat-widget/lib/styles.css';
 import HomeContainer from './components/containers/HomeContainer';
 import AboutContainer from './components/containers/AboutContainer';
 import PortfolioContainer from './components/containers/PortfolioContainer';
 import ContactContainer from './components/containers/ContactContainer';
+// eslint-disable-next-line
 import {AnimatedSwitch} from 'react-router-transition';
+
 
 class App extends Component {
 
@@ -26,16 +28,18 @@ class App extends Component {
     console.log("render");
     return (
       <Router>
-        <AnimatedSwitch 
+        <div className="App">
+        {/* <AnimatedSwitch 
         atEnter={{opacity:0}}
         atLeave={{opacity:0}}
         atActive={{opacity:1}}
-        className="App">
+        className="App"> */}
         <Route exact path="/" component={this.renderHome}></Route>
         <Route exact path="/quienessomos" component={this.renderAbout}></Route>
         <Route exact path="/portafolio" component={this.renderPortfolio}></Route>
         <Route exact path="/contacto" component={this.renderContact}></Route>
-        </AnimatedSwitch>
+        {/* </AnimatedSwitch> */}
+        </div>
       </Router>
     )
   };
