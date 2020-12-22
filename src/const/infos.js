a82890511515f457901607518de36ae6771aacfa
Luis Carlos Garcia Canchano <luisgarciasism@gmail.com>
2020-12-22 16:25:24 -0500

    delete blank lines & col prop in infoPortfolio

--- src/const/infos.js
+++ src/const/infos.js
@@ -21,19 +21,12 @@ import ultrasonido from './../img/portfolioImg/ultrasonido.jpg';
 export const infoPrincipios = [
 
   { id: 1, name: 'Solidaridad', description: 'Por nuestro sentido social y humano brindamos apoyo y atención a nuestros usuarios en forma inmediata, especialmente a los sectores más vulnerables de nuestra sociedad.' },
-
   { id: 2, name: 'Calidad', description: 'Buscamos la satisfacción de las necesidades de nuestros usuarios, de conformidad con la normatividad vigente y mejoramiento continuo de los procesos.' },
-
   { id: 3, name: 'Trabajo en equipo', description: 'Nos permite brindar a nuestros usuarios seguridad en que el trabajo realizado por nuestro talento humano se engrana para brindarles un servicio óptimo y oportuno; un equipo de trabajo multidisciplinario comprometido con un proceso continuo de mejoramiento de los estándares de calidad en la prestación de servicios de salud.'},
-
-  {id: 4, name: 'Amabilidad', description: 'Brindar a nuestros usuarios a través de la gentileza de nuestros empleados una atención en el servicio que se diferencie de otras entidades prestadoras de servicios de salud.'},
-
-  {id: 5, name: 'Ética', description: 'Constituida por valores morales que nos permiten adoptar decisiones y determinar un comportamiento apropiado. Esos valores están basados en lo que es correcto.' },
-
+  { id: 4, name: 'Amabilidad', description: 'Brindar a nuestros usuarios a través de la gentileza de nuestros empleados una atención en el servicio que se diferencie de otras entidades prestadoras de servicios de salud.'},
+  { id: 5, name: 'Ética', description: 'Constituida por valores morales que nos permiten adoptar decisiones y determinar un comportamiento apropiado. Esos valores están basados en lo que es correcto.' },
   { id: 6, name: 'Universalidad', description: 'Acorde a la normatividad existente y los recursos disponibles en nuestra empresa, los servicios que ofrecemos son prestados sin ningún tipo de discriminación racial, edad, sexo, idioma, clase social o credo.' },
-  
   { id: 7, name: 'Honestidad y transparencia', description: 'Son cualidades humanas por las que determinamos elegir y actuar siempre con base en la verdad y en la auténtica justicia (dando a cada quien lo que le corresponde, incluidos nosotros mismos), asumiendo que la verdad es solo una y que no depende de personas o consensos sino de lo que el mundo real nos presenta como innegable e imprescindible de reconocer.' },
-
   { id: 8, name: 'Participación social', description: 'Nuestros usuarios son nuestra razón de ser por ello siempre los tenemos en cuenta y a su entorno familiar; enfocándonos en la socialización de los programas que van dirigidos a mejorar su calidad de vida dependiendo de la necesidad de cada usuario; esto conlleva al mejoramiento de la calidad en la prestación del servicio de nuestra empresa.' },
 
   /* { id: 6, name: 'Eficiencia', description: 'Máxima productividad de los recursos asignados para agregar valor a los servicios prestados a la comunidad. ' },
@@ -50,30 +43,30 @@ export const infoPrincipios = [
 
 export const infoPortfolio = [
 
-  { col: 2, name: 'Medicina General', image: medicinageneral, description: '1' },
-  { col: 1, name: 'Medicina Interna', image: medicinainterna, description: '2' },
-  { col: 2, name: 'Ginescostetricia', image: ginescostetricia, description: '3' },
-  { col: 1, name: 'Pediatria', image: pediatria, description: '4' },
-  { col: 1, name: 'Enfermería', image: enfermeria, description: '5' },
-  { col: 1, name: 'Promoción en salud', image: promocionensalud, description: '6' },
-  { col: 1, name: 'Planificación familiar', image: planificacionfamiliar, description: '7' },
-  { col: 1, name: 'Nutrición y dietética', image: nutricionydietetica, description: '8' },
-  { col: 2, name: 'Psicología', image: psicologia, description: '9' },
-  { col: 2, name: 'Laboratorio clínico', image: laboratorioclinico, description: '10' },
-  { col: 1, name: 'Toma de muestras de laboratorio clínico', image: tomademuestradelaboratorioclinico, description: '11' },
-  { col: 1, name: 'Endocrinología', image: endocrinologia, description: '12' },
-  { col: 1, name: 'Gastroenterología', image: gastroenterologia, description: '13' },
-  { col: 1, name: 'Neumología', image: neumologia, description: '14' },
-  { col: 1, name: 'Neurología', image: neurologia, description: '15' },
-  { col: 1, name: 'Otorrinolaringología', image: otorrinolaringologia, description: '16' },
-  { col: 1, name: 'Urología', image: urologia, description: '17' },
-  { col: 1, name: 'Dermatología', image: dermatologia, description: '18' },
-  { col: 2, name: 'Ultrasonido', image: ultrasonido, description: '19' },
+  { name: 'Medicina General', image: medicinageneral, description: '1' },
+  { name: 'Medicina Interna', image: medicinainterna, description: '2' },
+  { name: 'Ginescostetricia', image: ginescostetricia, description: '3' },
+  { name: 'Pediatria', image: pediatria, description: '4' },
+  { name: 'Enfermería', image: enfermeria, description: '5' },
+  { name: 'Promoción en salud', image: promocionensalud, description: '6' },
+  { name: 'Planificación familiar', image: planificacionfamiliar, description: '7' },
+  { name: 'Nutrición y dietética', image: nutricionydietetica, description: '8' },
+  { name: 'Psicología', image: psicologia, description: '9' },
+  { name: 'Laboratorio clínico', image: laboratorioclinico, description: '10' },
+  { name: 'Toma de muestras de laboratorio clínico', image: tomademuestradelaboratorioclinico, description: '11' },
+  { name: 'Endocrinología', image: endocrinologia, description: '12' },
+  { name: 'Gastroenterología', image: gastroenterologia, description: '13' },
+  { name: 'Neumología', image: neumologia, description: '14' },
+  { name: 'Neurología', image: neurologia, description: '15' },
+  { name: 'Otorrinolaringología', image: otorrinolaringologia, description: '16' },
+  { name: 'Urología', image: urologia, description: '17' },
+  { name: 'Dermatología', image: dermatologia, description: '18' },
+  { name: 'Ultrasonido', image: ultrasonido, description: '19' },
 
 ]
 
 export const mision = "SERVICIOS INTEGRALES DE SALUD DEL MAGDALENA es una  Institución privada  Prestadora de Servicios de Salud  ambulatoria  del Nivel Primario; en el departamento del Magdalena; brindando un servicio seguro, pertinente, accesible, eficaz, oportuno y confiable, que permite cubrir las expectativas en salud, de los usuarios que los reciben; diseñados de acuerdo a los modelos de atención requeridos con el apoyo de un talento humano competente que permite mejorar la salud y su calidad de vida.";
 
-export const vision = "Ser reconocidos  como una de las mejores instituciones prestadoras de servicios de salud en el año 2025 en la región caribe, con una atención integral, responsable, segura y eficaz, logrando impactar en el buen comportamiento de la salud de nuestros usuarios, con respeto y ética, con una sostenibilidad financiera que permita contar con la tecnología y el recurso humano que merece la región.";
+export const vision = "Ser reconocidos  como una de las mejores instituciones prestadoras de servicios de salud en el año 2025 en la región caribe, con una atención integral, responsable, segura y eficaz, logrando impactar en el buen comportamiento de la salud de nuestros usuarios, con respeto y ética, con una sostenibilidad financiera que permita contar cozn la tecnología y el recurso humano que merece la región.";
 
 export const objetivos = "Garantizar un servicio de salud  eficiente, humanizado, oportuno y eficaz, que permita mejorar la calidad de vida de nuestros usuarios.";
